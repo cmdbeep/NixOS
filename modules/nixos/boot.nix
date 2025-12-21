e0cd7ec98b775f7070755a958ff81fa372eb1d2b
Finn H <hasse.finn@icloud.com>
2025-12-21 14:10:37 +0100

    changed boot file to a more dynamic and flexible look

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -5,16 +5,16 @@
   boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.kernel.sysctl."kernel.sysrq" = 244;
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
-
-  # boot.plymouth = {
-  #   enable = true;
-  #   # font = "${pkgs.maple-mono.Normal-NF}/share/fonts/MapleMono-NF-Regular.ttf";
-  #   theme = "bgrt";
-  #   themePackages = [ pkgs.nixos-bgrt-plymouth ];
-  # };
+  boot.loader = {
+    grub = {
+      enable = true;
+      device = "/dev/vda";
+      # device = "nodev";
+      # efiSupport = true;
+      useOSProber = true;
+    };
+    # efi.canTouchEfiVariables = true;
+  };
 
   services.earlyoom.enable = true;
 }
