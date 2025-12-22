b54c4ccd04c20db875df17eeba10ab7d667732ba
Finn H <hasse.finn@icloud.com>
2025-12-22 23:44:40 +0100

    chnages + some tweaking

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,19 +1,23 @@
 { pkgs, ... }:
 {
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  # boot.initrd.kernelModules = [ "amdgpu" ];
 
   boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.kernel.sysctl."kernel.sysrq" = 244;
 
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+  ];
+
   boot.loader = {
     grub = {
       enable = true;
-      device = "/dev/vda";
-      # device = "nodev";
-      # efiSupport = true;
-      useOSProber = true;
+      device = "nodev";
+      efiSupport = true;
     };
-    # efi.canTouchEfiVariables = true;
+    efi.canTouchEfiVariables = true;
+    efi.efiSysMountPoint = "/boot";
   };
 
   services.earlyoom.enable = true;
